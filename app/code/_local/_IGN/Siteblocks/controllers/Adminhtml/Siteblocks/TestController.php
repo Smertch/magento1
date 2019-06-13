cf9d7cb3bd6f40f10eb38190fcb048cc0a51eb75
Sergey P <sergeyp@ronis.mail>
2019-06-13 17:54:36 +0300

    del

--- app/code/_local/_IGN/Siteblocks/controllers/Adminhtml/Siteblocks/TestController.php
+++
@@ -1,8 +0,0 @@
-<?php
-class IGN_Siteblocks_Adminhtml_Siteblocks_TestController extends Mage_Adminhtml_Controller_Action {
-
-    public function mytestAction()
-    {
-        die('admin');
-    }
-}
